b0d230cdf191522d179146f46422fb7083189714
andrinja <andra.zeberga@gmail.com>
2019-10-09 20:44:12 +0200

    fetch data from server with getInitialProps, error page

---
+++ pages/_error.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout';
+
+export default () => (
+    <Layout title="Errorr!!">
+        <p>Couldn't het that page, sorry :(</p>
+    </Layout>
+)
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,46 @@
 import Link from "next/link";
 import Layout from "../components/layout";
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-export default () => (
-    <Layout title="About">
-        <Link href="/">
-        <a>Go home</a>
-        </Link>
-        <p>Dog lover</p>
-        <img src="/static/dog.jpeg" alt=""/>
-    </Layout>
-)
+export default class About extends Component {
+    // state = {
+    //     user: null
+    // }
+    // static means it can be executed within or outside the Class. Belongs to About class
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/reedbarger');
+        const data = await res.json();
+        // fetch('https://api.github.com/users/reedbarger')
+        // .then(res => res.json())
+        // .then(data=> {
+        //     console.log(data)
+        // })
+
+        return {  
+            user: data
+            // user: 'user'
+          }
+    }
+    // lifecycle hook
+    // componentDidMount() {
+    //     fetch('https://api.github.com/users/reedbarger')
+    //     .then(res => res.json())
+    //     .then(data=> {
+    //         this.setState({
+    //             user: data
+    //         })
+    //     })
+    // }
+   render() {
+       const { user } = this.props;
+       return(
+        <Layout title="About">
+             
+            {/* {JSON.stringify(this.state.user)} */}
+            <p>{user.name}</p>
+            <img src={user.avatar_url} alt="me"/>
+        </Layout>
+       )
+   }
+}
