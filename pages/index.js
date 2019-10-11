561b519c512741734f9f05a5f6f44fe3306271ce
andrinja <andra.zeberga@gmail.com>
2019-10-11 11:27:19 +0200

    post index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Link from "next/link";
+
 import Layout from "../components/layout";
 
 const Index = () => (
     <Layout title="Home">
-        <Link href="/about">
-            <a>Go to about</a>
-        </Link>
         <div>I fix your page.js</div>
     </Layout>
     
--- pages/post.js
+++ pages/post.js
@@ -3,9 +3,10 @@ import { withRouter } from 'next/router';
 
  const Post = ({ router }) => (
     <Layout title={ router.query.title }>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit repudiandae sapiente expedita amet unde quidem, error asperiores vitae enim nisi, saepe labore nemo doloremque dolore pariatur impedit, esse itaque.</p>
+    <p style={{ width: "80vw" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit repudiandae sapiente expedita amet unde quidem, error asperiores vitae enim nisi, saepe labore nemo doloremque dolore pariatur impedit, esse itaque.</p>
     
     </Layout>
 )
 
+// to get router prop 
 export default withRouter(Post);
